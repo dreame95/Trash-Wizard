58770ff4ef2ecc953db5f704ad8623090d229851
dreame95 <austinessinger@gmail.com>
2018-09-28 18:20:23 -0400

    reupload
    I had to reupload due to some major conflicts

---
+++ Assets/Scripts/_GM/MusicManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] lvlMusicArray;
+    public AudioSource audioSource;
+
+    void Awake(){
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destroy on load " + name);
+    }
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    private void OnLevelWasLoaded(int level){
+        AudioClip thisLvlMusic = lvlMusicArray[level];
+        Debug.Log("This lvls music" + thisLvlMusic);
+
+        if(thisLvlMusic){
+            // if there's music attached in array
+            audioSource.clip = thisLvlMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    public void SetVolume(float volume){
+        audioSource.volume = volume;
+    }
+}
---
+++ Assets/Scripts/_GM/OptionsController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class OptionsController : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public Toggle fullscreenToggle;
+    public Dropdown resolutionDropdown;
+
+    public Resolution[] resolutions;
+
+    public PlayerPrefsManager playerPrefsManager;
+    private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+        musicManager = GameObject.FindObjectOfType<MusicManager>();
+        volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+
+        fullscreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+
+        resolutions = Screen.resolutions;
+        foreach(Resolution resolution in resolutions){
+            resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        musicManager.SetVolume(volumeSlider.value);
+	}
+
+    public void SaveAndExit(){
+        PlayerPrefsManager.SetMasterVolume(volumeSlider.value);
+
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SetDefault(){
+        volumeSlider.value = 0.8f;
+    }
+
+    public void OnFullscreenToggle(){
+        playerPrefsManager.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    public void OnResolutionChange(){
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+    }
+}
